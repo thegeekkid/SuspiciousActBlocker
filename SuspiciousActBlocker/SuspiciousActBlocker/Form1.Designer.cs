7119f05943d9e2137816d1e09525a245fc87126d
thegeekkid <brian@bscc.support>
2017-07-08 17:32:15 -0500

    Set window to open center screen.

--- SuspiciousActBlocker/SuspiciousActBlocker/Form1.Designer.cs
+++ SuspiciousActBlocker/SuspiciousActBlocker/Form1.Designer.cs
@@ -162,6 +162,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Suspicious Activity Detected!";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
